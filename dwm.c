84abc2bbab8af09763124bd0989335a24f4dcad3
Jack Bawer <jackb2507@gmail.com>
2025-10-31 07:01:30 +0100

    killunsel (kill all visible in tag excpet focused one)

--- dwm.c
+++ dwm.c
@@ -227,6 +227,7 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
+static void killunsel(const Arg *arg);
 static void loadxrdb(void);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
@@ -1315,6 +1316,29 @@ loadxrdb()
   }
 
   XCloseDisplay(display);
+}
+
+void
+killunsel(const Arg *arg)
+{
+       Client *i = NULL;
+
+       if (!selmon->sel)
+               return;
+
+       for (i = selmon->clients; i; i = i->next) {
+               if (ISVISIBLE(i) && i != selmon->sel) {
+                       if (!sendevent(i->win, wmatom[WMDelete], NoEventMask, wmatom[WMDelete], CurrentTime, 0 , 0, 0)) {
+                               XGrabServer(dpy);
+                               XSetErrorHandler(xerrordummy);
+                               XSetCloseDownMode(dpy, DestroyAll);
+                               XKillClient(dpy, i->win);
+                               XSync(dpy, False);
+                               XSetErrorHandler(xerror);
+                               XUngrabServer(dpy);
+                       }
+               }
+       }
 }
 
 void
